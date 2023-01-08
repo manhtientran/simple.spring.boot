e4d1e888fb0ffe6431077b9ac116df2c41353503
Manh Tran <tienmanh.tran68@gmail.com>
2023-01-08 11:10:18 +0700

    Starting the server

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world!";
+	}
+
+	@GetMapping("/list")
+	public List<String> list() {
+		return List.of("Hello", "World");
+	}
 }
